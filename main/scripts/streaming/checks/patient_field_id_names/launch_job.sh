ca9c8b1b730d8497ee117715c09fb6b8a2015fa5
Stelios Gerogiannakis <sgerogia@gmail.com>
2014-04-05 06:44:33 -0700

    Name and file cleanup for patient_field_id_names job

--- main/scripts/streaming/checks/patient_field_id_names/launch_job.sh
+++ main/scripts/streaming/checks/patient_field_id_names/launch_job.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 hadoop jar $HADOOP_STREAMING \
 -D mapred.reduce.tasks=1  \
--D mapred.job.name="patient_field_ids_list" \
+-D mapred.job.name="patient_field_id_names" \
 -D stream.non.zero.exit.is.failure=false \
 -input $1 \
 -output patient_field_id_names \
--mapper "mapper.sh" -file "./main/scripts/streaming/checks/patient_field_ids_list/mapper.sh" \
--reducer "reducer.sh" -file "./main/scripts/streaming/checks/patient_field_ids_list/reducer.sh"
+-mapper "mapper.sh" -file "./main/scripts/streaming/checks/patient_field_id_names/mapper.sh" \
+-reducer "reducer.sh" -file "./main/scripts/streaming/checks/patient_field_id_names/reducer.sh"
+
+rm -f mapper.sh
+rm -f reducer.sh
